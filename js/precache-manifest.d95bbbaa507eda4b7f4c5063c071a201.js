7ff9152297f0f8d55316c3ab82a75702b844c47a
hiCasper <hiCasper@users.noreply.github.com>
2022-08-01 14:31:10 +0000

    Deploying to dist from @ MoeTools/sub-web@43f57b6fd05804724b0746ecfdaed7475b923d28 🚀

---
+++ js/precache-manifest.d95bbbaa507eda4b7f4c5063c071a201.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "/subweb/_redirects"
+  },
+  {
+    "revision": "74e468ff679c828aad62",
+    "url": "/subweb/css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "de133d5e8a2bbb7ff973",
+    "url": "/subweb/css/chunk-vendors.53794358.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/subweb/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/subweb/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d6d8d682b435bc81f552a2b277fbfb37",
+    "url": "/subweb/index.html"
+  },
+  {
+    "revision": "74e468ff679c828aad62",
+    "url": "/subweb/js/app.81b2f753.js"
+  },
+  {
+    "revision": "f56033d5e15e60928dbb",
+    "url": "/subweb/js/chunk-652681bc.e6bb882b.js"
+  },
+  {
+    "revision": "de133d5e8a2bbb7ff973",
+    "url": "/subweb/js/chunk-vendors.f351a9eb.js"
+  },
+  {
+    "revision": "3fa7348f187dae70fad03ee4cc4e0a4f",
+    "url": "/subweb/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/subweb/robots.txt"
+  }
+]);
